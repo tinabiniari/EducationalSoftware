b9b59da28ffc25cbb8cb90388d459736b35c5428
Tina Biniari <tina.bn.23@gmail.com>
2019-04-13 21:59:03 +0300

    initial commit

---
+++ src/main/java/com/software/educational/EducationalApplication.java
@@ -0,0 +1,13 @@
+package com.software.educational;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationalApplication.class, args);
+    }
+
+}
